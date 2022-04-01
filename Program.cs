3c7987c0d45de74a45ac7144f39628094319667e
JAnderson217 <24385176+JAnderson217@users.noreply.github.com>
2022-04-01 13:33:43 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,29 +8,25 @@ namespace ExampleTest
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            //declarations
+            //Integer, Strings, Char, Boolean, UInt, int8, int16, int32, float, Double
+            
             for (int i = 0; i < 10; i++)
             {
-              //  Console.WriteLine(i + ": Hello World");
-              //  Console.WriteLine("{0}: Hello World", i);
                 Console.WriteLine($"{i}: Hello World");
             }
-
             //Instantiation
             Mathfunction arith = new Mathfunction();
             int sum = arith.Add(3, 6);
             Console.WriteLine();
             Console.WriteLine($"Sum is: {sum}");
             Console.WriteLine("12+32 is: " + arith.Add(12,32));
-
             int product = Multiply(3, 6);
             Console.WriteLine("Product: " + product);
-            
-            
             Console.ReadLine();
-
-
         }
 
         public static int Multiply(int v1, int v2)
